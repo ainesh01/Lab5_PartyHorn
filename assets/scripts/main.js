7a5a61b14bbe18d7b9032f4a803cbbfe4adbe422
Ainesh Arumugam <ainesh01@gmail.com>
2021-02-05 18:36:57 -0800

    implemented audio

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,7 +3,17 @@
 const volField = document.getElementById('volume-number');
 const volSlider = document.getElementById('volume-slider');
 const volIcon = document.getElementById('volume-image');
+const audioBut = document.getElementById('honk-btn');
+const airRad = document.getElementById('radio-air-horn');
+const carRad = document.getElementById('radio-car-horn');
+const partyRad = document.getElementById('radio-party-horn');
+const soundImage = document.getElementById('sound-image');
 
+const horn = {
+    airHorn: 'air',
+    carHorn: 'car',
+    partyHorn: 'party',
+}
 
 
 let volume;
@@ -11,6 +21,8 @@ let volume;
 
 volField.addEventListener('input', updateVolume);
 volSlider.addEventListener('input', updateVolume);
+audioBut.addEventListener('click', playAudio);
+
 
 function updateVolume(e) {
     volume = e.target.value;
@@ -37,5 +49,51 @@ function updateVolume(e) {
 
 
 
+airRad.addEventListener('input', () => setHorn(air));
+carRad.addEventListener('input', () => setHorn(car));
+partyRad.addEventListener('input', () => setHorn(party));
 
-// TODO
+const setHorn = (horn) => {
+    if (horn == 'air'){
+        document.getElementById('horn-sound').src = "./assets/media/audio/air-horn.mp3";
+        soundImage.src = mediaDir + '/images/air-horn.svg';
+    }
+    else if (horn == 'car') {
+        document.getElementById('horn-sound').src = "./assets/media/audio/car-horn.mp3";
+        soundImage.src = mediaDir + '/images/car.svg';
+    }
+    else if (horn == 'air') {
+        document.getElementById('horn-sound').src = "./assets/media/audio/party-horn.mp3";
+        soundImage.src = mediaDir + '/images/party-horn.svg';
+    }
+    
+}
+
+const setHornSound = (horn) => {
+    
+    document.getElementById('horn-sound').src = `${mediaDir}/audio/${horn}-horn.mp3`
+}
+
+const setHornImage = (horn) => {
+    const soundImage = document.getElementById('sound-image');
+    if (horn === Horns.AIR) {
+        soundImage.src = mediaDir + '/images/air-horn.svg';
+    } else if (horn === Horns.CAR) {
+        soundImage.src = mediaDir + '/images/car.svg'
+    } else {
+        soundImage.src = mediaDir + '/images/party-horn.svg'
+    }
+}
+
+
+
+
+audioBut.onclick = (event) => playSound(event);
+
+
+const playSound = (event) => {
+    event.preventDefault();
+    document.getElementById('horn-sound').play();
+}
+
+
