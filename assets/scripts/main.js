bb256a6461428e1287b0ebee9493e809acc5aaf1
Ainesh Arumugam <ainesh01@gmail.com>
2021-02-05 18:15:48 -0800

    reenable button when not 0

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -13,21 +13,24 @@ volField.addEventListener('input', updateVolume);
 volSlider.addEventListener('input', updateVolume);
 
 function updateVolume(e) {
-    volume =  e.target.value;
+    volume = e.target.value;
     document.getElementById('volume-number').value = volume;
     document.getElementById('volume-slider').value = volume;
-    if(volume == 0){
+    if (volume == 0) {
         volIcon.src = "./assets/media/icons/volume-level-0.svg"
-        document.getElementById("honk-btn").disabled = true; 
+        document.getElementById("honk-btn").disabled = true;
     }
     else if (volume >= 1 && volume <= 33) {
         volIcon.src = "./assets/media/icons/volume-level-1.svg"
+        document.getElementById("honk-btn").disabled = false;
     }
     else if (volume >= 34 && volume <= 66) {
         volIcon.src = "./assets/media/icons/volume-level-2.svg"
+        document.getElementById("honk-btn").disabled = false;
     }
     else if (volume >= 67 && volume <= 100) {
         volIcon.src = "./assets/media/icons/volume-level-3.svg"
+        document.getElementById("honk-btn").disabled = false;
     }
 }
 
