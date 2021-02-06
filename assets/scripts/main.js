382c4edcaebb35b774098578cd1fd56cbac416f4
Ainesh Arumugam <ainesh01@gmail.com>
2021-02-05 18:02:49 -0800

    added image switching

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,8 @@
 // main.js
 
-var volField = document.getElementById('volume-number');
-var volSlider = document.getElementById('volume-slider');
+const volField = document.getElementById('volume-number');
+const volSlider = document.getElementById('volume-slider');
+const volIcon = document.getElementById('volume-image');
 
 
 
@@ -15,6 +16,18 @@ function updateVolume(e) {
     volume =  e.target.value;
     document.getElementById('volume-number').value = volume;
     document.getElementById('volume-slider').value = volume;
+    if(volume == 0){
+        volIcon.src = "./assets/media/icons/volume-level-0.svg"
+    }
+    else if (volume >= 1 && volume <= 33) {
+        volIcon.src = "./assets/media/icons/volume-level-1.svg"
+    }
+    else if (volume >= 34 && volume <= 66) {
+        volIcon.src = "./assets/media/icons/volume-level-2.svg"
+    }
+    else if (volume >= 67 && volume <= 100) {
+        volIcon.src = "./assets/media/icons/volume-level-3.svg"
+    }
 }
 
 
