86ea322570d72bd3806abbec6bc542d954164f85
Ainesh Arumugam <ainesh01@gmail.com>
2021-02-05 17:28:40 -0800

    linked text and slider

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,24 @@
 // main.js
 
+var volField = document.getElementById('volume-number');
+var volSlider = document.getElementById('volume-slider');
+
+
+
+let volume;
+
+
+volField.addEventListener('input', updateVolume);
+volSlider.addEventListener('input', updateVolume);
+
+function updateVolume(e) {
+    volume =  e.target.value;
+    document.getElementById(volField).value = volume;
+    document.getElementById(volSlider).value = volume;
+}
+
+
+
+
+
 // TODO
